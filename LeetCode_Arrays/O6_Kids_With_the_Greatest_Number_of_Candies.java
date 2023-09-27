cf12074eb3438041282770b8bf61d6404a9cce9b
Abkaplesh02 <abkaplesh02@gmail.com>
2023-09-27 08:41:37 +0530

    add codes leetcode

---
+++ LeetCode_Arrays/O6_Kids_With_the_Greatest_Number_of_Candies.java
@@ -0,0 +1,29 @@
+//1431. Kids With the Greatest Number of Candies
+//
+//        There are n kids with candies. You are given an integer array candies, where each candies[i] represents the number of candies the ith kid has, and an integer extraCandies, denoting the number of extra candies that you have.
+//
+//        Return a boolean array result of length n, where result[i] is true if, after giving the ith kid all the extraCandies, they will have the greatest number of candies among all the kids, or false otherwise.
+//
+//        Note that multiple kids can have the greatest number of candies.
+
+
+//class Solution {
+//    public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+//        int Great=0;
+//        for(int i=0;i<candies.length;i++){
+//            Great=Math.max(candies[i],Great);
+//        }
+//        ArrayList<Boolean> List=new ArrayList<>();
+//        for(int i=0;i<candies.length;i++){
+//            if(extraCandies+candies[i]>=Great){
+//                List.add(true);
+//            }
+//            else{
+//                List.add(false);
+//            }
+//        }
+//
+//        return List;
+//
+//    }
+//}
