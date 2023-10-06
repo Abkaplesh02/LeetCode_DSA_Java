57844deb2572660aa8f37e0d54abba0e231106fb
Abkaplesh02 <abkaplesh02@gmail.com>
2023-10-06 12:00:27 +0530

    update fiiles

--- LeetCode_Arrays/O10_Check_if_the_Sentence_Is_Pangram_1832.java
+++ LeetCode_Arrays/O10_Check_if_the_Sentence_Is_Pangram_1832.java
@@ -7,25 +7,23 @@ import java.util.Arrays;
 //A pangram is a sentence where every letter of the English alphabet appears at least once.
 //
 //Given a string sentence containing only lowercase English letters, return true if sentence is a pangram, or false otherwise.
-//class Solution {
-//class Solution {
-//    public boolean checkIfPangram(String sentence) {
-//        // boolean Freq[]=new int[26];
-//
-//Method 1
-//        boolean ans=false;
-//        for(char alpha ='a';alpha<='z';alpha++){
-//            if(sentence.indexOf(alpha)==-1){
-//                return false;
-//
-//            }
-//        }
-//        return true;
+class Solution {
+    public boolean checkIfPangram(String sentence) {
+        // boolean Freq[]=new int[26];
+        
+        
+        for(char alpha ='a';alpha<='z';alpha++){
+            if(sentence.indexOf(alpha)==-1){
+                return false;
+               
+            }
+        }
+        return true;
 
 
 
 
-//Method 2
+
 
         // int Freq[]=new int [123];
         //  Arrays.fill(Freq,0);
@@ -33,12 +31,12 @@ import java.util.Arrays;
         // for(int i=0;i<sentence.length();i++){
         //     Freq[sentence.charAt(i)]++;
         // }
-
+        
         // for(int i=97;i<=122;i++){
         //     if(Freq[i]==0){
         //         return false;
         //     }
         // }
         // return true;
-//    }
-//}
+    }
+}
