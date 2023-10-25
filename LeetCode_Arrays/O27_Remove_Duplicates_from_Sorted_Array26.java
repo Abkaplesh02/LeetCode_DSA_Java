c44a6d215a5a443d9b36b32308cf04bc0499eb07
Abkaplesh02 <abkaplesh02@gmail.com>
2023-10-25 06:24:50 +0530

    add leetCode

--- LeetCode_Arrays/O27_Remove_Duplicates_from_Sorted_Array26.java
+++ LeetCode_Arrays/O27_Remove_Duplicates_from_Sorted_Array26.java
@@ -21,17 +21,17 @@
 //If all assertions pass, then your solution will be accepted.
 //
 //
-//class Solution {
-//    public int removeDuplicates(int[] nums) {
-//        int n=nums.length;
-//        int i=0;
-//        for(int j=1;j<n;j++){
-//            if(nums[j]!=nums[i]){
-//                nums[i+1]=nums[j];
-//                i++;
-//            }
-//        }
-//        return i+1;
-//
-//    }
-//}
+class Solution {
+   public int removeDuplicates(int[] nums) {
+       int n=nums.length;
+       int i=0;
+       for(int j=1;j<n;j++){
+           if(nums[j]!=nums[i]){
+               nums[i+1]=nums[j];
+               i++;
+           }
+       }
+       return i+1;
+
+   }
+}
